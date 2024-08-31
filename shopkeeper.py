355404a1ea917431ccb28052297452fc2154c173
rakshithayv1609 <rakshithayvrakshitha21@gmail.com>
2024-08-31 12:31:34 +0530

    Code files uploded

---
+++ shopkeeper.py
@@ -0,0 +1,31 @@
+def add_chocolates(chocolates):
+  name=input("Enter name of the chocolates: ")
+  amount=int(input("Enter the amount: "))
+  stock=int(input("Enter the stocks: "))
+  chocolate={'name':name,'amount':amount,'stock':stock}
+  chocolates.append(chocolate)
+  return chocolates
+
+def add_chocolates(chocolates):
+  name=input("Enter name of the chocolates: ")
+  amount=int(input("Enter the amount: "))
+  stock=int(input("Enter the stocks: "))
+  chocolate={'name':name,'amount':amount,'stock':stock}
+  chocolates.append(chocolate)
+  return chocolates
+
+def add_chocolates(chocolates):
+  name=input("Enter name of the chocolates: ")
+  amount=int(input("Enter the amount: "))
+  stock=int(input("Enter the stocks: "))
+  chocolate={'name':name,'amount':amount,'stock':stock}
+  chocolates.append(chocolate)
+  return chocolates
+
+def add_chocolates(chocolates):
+  name=input("Enter name of the chocolates: ")
+  amount=int(input("Enter the amount: "))
+  stock=int(input("Enter the stocks: "))
+  chocolate={'name':name,'amount':amount,'stock':stock}
+  chocolates.append(chocolate)
+  return chocolates
